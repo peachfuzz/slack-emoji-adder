367352a8ce5ca4730fa061104863d02e19449750
Hector <hectordm65@gmail.com>
2019-03-15 12:45:18 -0700

    geckodriver isn't working, idk why 🤷‍♀️

---
+++ readme.md
@@ -0,0 +1,17 @@
+#Slack Emoji Adder
+This script should use your slack credentials and slack workspace to automatically create emojis from images in a desired folder.
+
+#Requirements
+You will need selenium
+install it with
+
+```bash
+pip install selenium
+```
+
+You will need to have driver for firefox.
+Install it with
+
+```bash
+brew install geckodriver
+```
--- slack-emoji-adder.py
+++ slack-emoji-adder.py
@@ -6,16 +6,29 @@ from selenium.webdriver.support.ui import WebDriverWait
 
 import json
 
-# get credendials from the json file 
-with open('creds.json') as json_data_file: # either rename the creds-template.json file to creds.json or change this line to say creds-template.json
+# get credendials from the json file
+# either rename the creds-template.json file to creds.json or change this line to say creds-template.json
+with open('creds.json') as json_data_file:
     data = json.load(json_data_file)
 
-# set your credentials here so your username and password isn't inline code :)
+# set your credentials and workspace here so your username and password isn't inline code :)
 username = data["username"]
 password = data["password"]
+workspace = data["workspace"] + "/customize/emoji"
 
-wait = WebDriverWait(driver, 10)
-wait.until(lambda driver: driver.current_url != "https://comparchwi19.slack.com/customize/emoji")
+# the driver isn't working and idk why
+# already tried (per: https://www.dev2qa.com/how-to-resolve-webdriverexception-geckodriver-executable-needs-to-be-in-path/):
+# brew install geckodriver #installed geckodriver
+# geckodriver --version    #verified it was installed
+# which geckodriver        #verified it was in my path
+# 🤷‍♀️
+# driver = webdriver.Firefox()
+# # driver = webdriver.Firefox(executable_path='/usr/local/bin/geckodriver')
+# driver.get(workspace)
 
-# driver = webdriver.Chrome()
-# driver.get("https://comparchwi19.slack.com/customize/emoji")
+# try:
+#     wait = WebDriverWait(driver, 10)
+#     wait.until(lambda driver: driver.current_url !=
+#                "https://comparchwi19.slack.com/customize/emoji")
+# finally:
+#     driver.quit()
