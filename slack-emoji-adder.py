c319f51ceb8fbe9add14ce741ea0735a9a9809c5
peachfuzz <hectordm65@gmail.com>
2019-03-15 10:39:39 -0700

    getting the ball rolling...

---
+++ slack-emoji-adder.py
@@ -0,0 +1,8 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+import json
+
+
+driver = webdriver.Firefox()
+driver.get("https://comparchwi19.slack.com/customize/emoji")
