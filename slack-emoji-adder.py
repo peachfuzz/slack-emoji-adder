42e07dd4d24db6ca977316e907b76088ef950eb1
peachfuzz <hectordm65@gmail.com>
2019-03-15 11:11:40 -0700

    handling redirects and added credendials template

--- slack-emoji-adder.py
+++ slack-emoji-adder.py
@@ -1,8 +1,21 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+
 import json
 
+# get credendials from the json file 
+with open('creds.json') as json_data_file: # either rename the creds-template.json file to creds.json or change this line to say creds-template.json
+    data = json.load(json_data_file)
 
-driver = webdriver.Firefox()
-driver.get("https://comparchwi19.slack.com/customize/emoji")
+# set your credentials here so your username and password isn't inline code :)
+username = data["username"]
+password = data["password"]
+
+wait = WebDriverWait(driver, 10)
+wait.until(lambda driver: driver.current_url != "https://comparchwi19.slack.com/customize/emoji")
+
+# driver = webdriver.Chrome()
+# driver.get("https://comparchwi19.slack.com/customize/emoji")
